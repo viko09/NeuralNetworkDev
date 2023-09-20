271d22a28f890d0abda057cd2efdacc646e62e16
Viko Luna <vm.luna912@gmail.com>
2023-09-20 14:48:04 -0600

    Red con optimizador RMSprop, falta verificarla y correrla.

---
+++ network_with_RMSprop.py
@@ -0,0 +1,94 @@
+import random
+import numpy as np
+
+
+class Network(object):
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+        # Inicializamos una nueva variable para RMSprop
+        self.square_gradients = [np.zeros(w.shape) for w in self.weights]
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a) + b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        training_data = list(training_data)
+        n = len(training_data)
+
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k + mini_batch_size]
+                            for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {} : {} / {}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {} complete!!".format(j))
+
+    def update_mini_batch(self, mini_batch, eta, rho=0.9, epsilon=1e-8):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        # Update weights and biases using RMSprop
+        self.weights = [w - (eta / len(mini_batch)) * nw / (np.sqrt(sw) + epsilon)
+                        for w, nw, sw in zip(self.weights, nabla_w, self.square_gradients)]
+        self.biases = [b - (eta / len(mini_batch)) * nb / (np.sqrt(sb) + epsilon)
+                       for b, nb, sb in zip(self.biases, nabla_b, self.square_gradients)]
+
+        # Update square gradients
+        self.square_gradients = [rho * sg + (1 - rho) * np.square(nw)
+                                 for sg, nw in zip(self.square_gradients, nabla_w)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # Feedforward
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+        # Backward pass
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return nabla_b, nabla_w
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return output_activations - y
+
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
